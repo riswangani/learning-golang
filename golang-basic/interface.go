1f05805f111df72825ec053b56a6ee96aad94a20
Riswan Gani Padilah <riswangani11@gmail.com>
2026-09-13 23:24:31 +0700

    interface

--- golang-basic/interface.go
+++ golang-basic/interface.go
@@ -4,11 +4,47 @@ import (
 	"fmt"
 )
 
-type Person interface {
-	Name() string
-	Age() int
+type HasName interface {
+	GetName() string
+	GetAge() int
+}
+
+func SayHello(value HasName) {
+	fmt.Println("Hello", value.GetName())
+	fmt.Println("Age", value.GetAge())
+}
+
+type Person struct {
+	Name string
+	// Address string
+	Age int
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+func (person Person) GetAge() int {
+	return person.Age
+}
+
+
+type Animal struct {
+	Name string
+	Age  int
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func (animal Animal) GetAge() int {
+	return animal.Age
 }
 
 func main() {
-
+	person := Person{Name: "Gani", Age: 30}
+	animal := Animal{Name: "Kucing", Age: 0}
+	SayHello(person)
+	SayHello(animal)
 }
